e75fafa5e1a45756465d85badfee51393de14b5b
Marco Colia <info@justoverclock.it>
2022-03-22 14:55:15 +0100

    try 2

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -1,2 +1,189 @@
-(()=>{var t={n:o=>{var e=o&&o.__esModule?()=>o.default:()=>o;return t.d(e,{a:e}),e},d:(o,e)=>{for(var n in e)t.o(e,n)&&!t.o(o,n)&&Object.defineProperty(o,n,{enumerable:!0,get:e[n]})},o:(t,o)=>Object.prototype.hasOwnProperty.call(t,o),r:t=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})}},o={};(()=>{"use strict";t.r(o);const e=flarum.core.compat["forum/app"];var n=t.n(e);const r=flarum.core.compat["forum/components/DiscussionList"];var s=t.n(r);const a=flarum.core.compat["common/extend"],d=flarum.core.compat["forum/components/IndexPage"];var i=t.n(d);n().initializers.add("justoverclock/random-post",(function(){(0,a.extend)(s().prototype,"oncreate",(function(){var t=this;if(n().current.matches(i())){var o=n().forum.attribute("justoverclock-random-post.charLimitRandomPost")||280;n().store.find("posts",{include:"discussion,user"}).then((function(e){t.randomPost=e[Math.floor(Math.random()*e.length)],document.getElementById("random-post-text").innerHTML=t.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g,"").substr(0,o);var r=document.getElementById("random-post-author"),s=n().route.user(t.randomPost.user()),a=t.randomPost.user().displayName(),d=t.randomPost.discussion().data.attributes.title,i=n().route.discussion(t.randomPost.discussion());r.innerHTML="<a href="+s+">"+a+"</a> in the <a href="+i+">"+d+"</a> "})),window.addEventListener("DOMContentLoaded",(function(t){var o=document.querySelectorAll(".DiscussionListItem").item(0);console.log(o);var e=document.createElement("div");e.innerHTML='<div id="random-post-one"><div class="random-post-content">\n        <p class="random-post-text" id="random-post-text"></p>\n        <div class="random-post-info">\n           <div class="random-post-author" id="random-post-author">\n        </div>\n      </div></div>',o.appendChild(e)}))}})),(0,a.extend)(s().prototype,"oncreate",(function(){var t=this;if(n().current.matches(i())){var o=n().forum.attribute("justoverclock-random-post.charLimitRandomPost")||280;n().store.find("posts",{include:"discussion,user"}).then((function(e){m.redraw(),t.randomPost=e[Math.floor(Math.random()*e.length)],document.getElementById("random-post-text-two").innerHTML=t.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g,"").substr(0,o);var r=document.getElementById("random-post-author-two"),s=n().route.user(t.randomPost.user()),a=t.randomPost.user().displayName(),d=t.randomPost.discussion().data.attributes.title,i=n().route.discussion(t.randomPost.discussion());r.innerHTML="<a href="+s+">"+a+"</a> in the <a href="+i+">"+d+"</a> "})),window.addEventListener("DOMContentLoaded",(function(t){var o=document.querySelectorAll(".DiscussionListItem").item(4);console.log(o);var e=document.createElement("div");e.innerHTML='<div id="random-post-two"><div class="random-post-content">\n        <p class="random-post-text" id="random-post-text-two"></p>\n        <div class="random-post-info">\n           <div class="random-post-author" id="random-post-author-two">\n        </div>\n      </div></div>',o.appendChild(e)}))}})),(0,a.extend)(s().prototype,"oncreate",(function(){var t=this;if(n().current.matches(i())){var o=n().forum.attribute("justoverclock-random-post.charLimitRandomPost")||280;n().store.find("posts",{include:"discussion,user"}).then((function(e){m.redraw(),t.randomPost=e[Math.floor(Math.random()*e.length)],document.getElementById("random-post-text-three").innerHTML=t.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g,"").substr(0,o);var r=document.getElementById("random-post-author-three"),s=n().route.user(t.randomPost.user()),a=t.randomPost.user().displayName(),d=t.randomPost.discussion().data.attributes.title,i=n().route.discussion(t.randomPost.discussion());r.innerHTML="<a href="+s+">"+a+"</a> in the <a href="+i+">"+d+"</a> "})),window.addEventListener("DOMContentLoaded",(function(t){var o=document.querySelectorAll(".DiscussionListItem").item(8);console.log(o);var e=document.createElement("div");e.innerHTML='<div id="random-post-two"><div class="random-post-content">\n        <p class="random-post-text" id="random-post-text-three"></p>\n        <div class="random-post-info">\n           <div class="random-post-author" id="random-post-author-three">\n        </div>\n      </div></div>',o.appendChild(e)}))}})),(0,a.extend)(s().prototype,"oncreate",(function(){var t=this;if(n().current.matches(i())){var o=n().forum.attribute("justoverclock-random-post.charLimitRandomPost")||280;n().store.find("posts",{include:"discussion,user"}).then((function(e){m.redraw(),t.randomPost=e[Math.floor(Math.random()*e.length)],document.getElementById("random-post-text-four").innerHTML=t.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g,"").substr(0,o);var r=document.getElementById("random-post-author-four"),s=n().route.user(t.randomPost.user()),a=t.randomPost.user().displayName(),d=t.randomPost.discussion().data.attributes.title,i=n().route.discussion(t.randomPost.discussion());r.innerHTML="<a href="+s+">"+a+"</a> in the <a href="+i+">"+d+"</a> "})),window.addEventListener("DOMContentLoaded",(function(t){var o=document.querySelectorAll(".DiscussionListItem").item(12);console.log(o);var e=document.createElement("div");e.innerHTML='<div id="random-post-two"><div class="random-post-content">\n        <p class="random-post-text" id="random-post-text-four"></p>\n        <div class="random-post-info">\n           <div class="random-post-author" id="random-post-author-four">\n        </div>\n      </div></div>',o.appendChild(e)}))}})),(0,a.extend)(s().prototype,"oncreate",(function(){var t=this;if(n().current.matches(i())){var o=n().forum.attribute("justoverclock-random-post.charLimitRandomPost")||280;n().store.find("posts",{include:"discussion,user"}).then((function(e){m.redraw(),t.randomPost=e[Math.floor(Math.random()*e.length)],document.getElementById("random-post-text-five").innerHTML=t.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g,"").substr(0,o);var r=document.getElementById("random-post-author-five"),s=n().route.user(t.randomPost.user()),a=t.randomPost.user().displayName(),d=t.randomPost.discussion().data.attributes.title,i=n().route.discussion(t.randomPost.discussion());r.innerHTML="<a href="+s+">"+a+"</a> in the <a href="+i+">"+d+"</a> "})),window.addEventListener("DOMContentLoaded",(function(t){var o=document.querySelectorAll(".DiscussionListItem").item(16);console.log(o);var e=document.createElement("div");e.innerHTML='<div id="random-post-two"><div class="random-post-content">\n        <p class="random-post-text" id="random-post-text-five"></p>\n        <div class="random-post-info">\n           <div class="random-post-author" id="random-post-author-five">\n        </div>\n      </div></div>',o.appendChild(e)}))}}))}))})(),module.exports=o})();
+/******/ (() => { // webpackBootstrap
+/******/ 	// runtime can't be in strict mode because a global variable is assign and maybe created.
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/forum/index.js":
+/*!****************************!*\
+  !*** ./src/forum/index.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_forum_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/forum/app */ "flarum/forum/app");
+/* harmony import */ var flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_forum_app__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var flarum_forum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flarum/forum/components/DiscussionList */ "flarum/forum/components/DiscussionList");
+/* harmony import */ var flarum_forum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flarum_forum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! flarum/common/extend */ "flarum/common/extend");
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(flarum_common_extend__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var flarum_forum_components_IndexPage__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! flarum/forum/components/IndexPage */ "flarum/forum/components/IndexPage");
+/* harmony import */ var flarum_forum_components_IndexPage__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(flarum_forum_components_IndexPage__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().initializers.add('justoverclock/random-post', function () {
+  (0,flarum_common_extend__WEBPACK_IMPORTED_MODULE_2__.extend)((flarum_forum_components_DiscussionList__WEBPACK_IMPORTED_MODULE_1___default().prototype), 'oncreate', function () {
+    var _this = this;
+
+    if (flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().current.matches((flarum_forum_components_IndexPage__WEBPACK_IMPORTED_MODULE_3___default()))) {
+      var charLim = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
+      flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().store.find('posts', {
+        include: 'discussion,user'
+      }).then(function (results) {
+        _this.randomPost = results[Math.floor(Math.random() * results.length)];
+        var randomPostTitle = document.getElementById('random-post-text');
+        randomPostTitle.innerHTML = _this.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g, '').substr(0, charLim);
+        var randomAuthor = document.getElementById('random-post-author');
+        var link = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().route.user(_this.randomPost.user());
+
+        var user = _this.randomPost.user().displayName();
+
+        var discussion = _this.randomPost.discussion().data.attributes.title;
+
+        var discussionLink = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default().route.discussion(_this.randomPost.discussion());
+        randomAuthor.innerHTML = "<a href=" + link + ">" + user + "</a> in the <a href=" + discussionLink + ">" + discussion + "</a> ";
+      });
+      window.addEventListener('DOMContentLoaded', function (event) {
+        var elements = document.querySelectorAll('.DiscussionListItem');
+        var firstEl = elements.item(0); //
+
+        console.log(firstEl);
+        var newEl = document.createElement('div');
+        newEl.innerHTML = '<div id="random-post-one"><div class="random-post-content">\n' + '        <p class="random-post-text" id="random-post-text"></p>\n' + '        <div class="random-post-info">\n' + '           <div class="random-post-author" id="random-post-author">\n' + '        </div>\n' + '      </div>' + '</div>';
+        firstEl.appendChild(newEl);
+      });
+    }
+  });
+});
+
+/***/ }),
+
+/***/ "flarum/common/extend":
+/*!******************************************************!*\
+  !*** external "flarum.core.compat['common/extend']" ***!
+  \******************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['common/extend'];
+
+/***/ }),
+
+/***/ "flarum/forum/app":
+/*!**************************************************!*\
+  !*** external "flarum.core.compat['forum/app']" ***!
+  \**************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['forum/app'];
+
+/***/ }),
+
+/***/ "flarum/forum/components/DiscussionList":
+/*!************************************************************************!*\
+  !*** external "flarum.core.compat['forum/components/DiscussionList']" ***!
+  \************************************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['forum/components/DiscussionList'];
+
+/***/ }),
+
+/***/ "flarum/forum/components/IndexPage":
+/*!*******************************************************************!*\
+  !*** external "flarum.core.compat['forum/components/IndexPage']" ***!
+  \*******************************************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = flarum.core.compat['forum/components/IndexPage'];
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!******************!*\
+  !*** ./forum.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_forum__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/forum */ "./src/forum/index.js");
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
 //# sourceMappingURL=forum.js.map
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,10 +1,11 @@
 import app from 'flarum/forum/app';
 import DiscussionList from 'flarum/forum/components/DiscussionList';
-import { extend } from 'flarum/common/extend';
+import {extend} from 'flarum/common/extend';
 import IndexPage from "flarum/forum/components/IndexPage";
 
 app.initializers.add('justoverclock/random-post', () => {
-extend(DiscussionList.prototype, 'oncreate', function () {
+
+  extend(DiscussionList.prototype, 'oncreate', function () {
     if (app.current.matches(IndexPage)) {
 
       const charLim = app.forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
@@ -39,175 +40,6 @@ extend(DiscussionList.prototype, 'oncreate', function () {
           '        <p class="random-post-text" id="random-post-text"></p>\n' +
           '        <div class="random-post-info">\n' +
           '           <div class="random-post-author" id="random-post-author">\n' +
-          '        </div>\n' +
-          '      </div>' +
-          '</div>';
-        firstEl.appendChild(newEl);
-      });
-    }
-  });
-  extend(DiscussionList.prototype, 'oncreate', function () {
-    if (app.current.matches(IndexPage)) {
-
-      const charLim = app.forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
-
-      app.store
-        .find('posts', {
-          include: 'discussion,user',
-        })
-        .then((results) => {
-          m.redraw();
-          this.randomPost = results[Math.floor(Math.random() * results.length)];
-
-
-          const randomPostTitle = document.getElementById('random-post-text-two');
-          randomPostTitle.innerHTML = this.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g, '').substr(0, charLim);
-
-          const randomAuthor = document.getElementById('random-post-author-two');
-          const link = app.route.user(this.randomPost.user());
-          const user = this.randomPost.user().displayName();
-          const discussion = this.randomPost.discussion().data.attributes.title;
-          const discussionLink = app.route.discussion(this.randomPost.discussion());
-          randomAuthor.innerHTML = `<a href=${link}>${user}</a> in the <a href=${discussionLink}>${discussion}</a> `;
-        });
-
-      window.addEventListener('DOMContentLoaded', (event) => {
-        const elements = document.querySelectorAll('.DiscussionListItem');
-        const firstEl = elements.item(4); //
-        console.log(firstEl);
-
-        const newEl = document.createElement('div');
-        newEl.innerHTML =
-          '<div id="random-post-two"><div class="random-post-content">\n' +
-          '        <p class="random-post-text" id="random-post-text-two"></p>\n' +
-          '        <div class="random-post-info">\n' +
-          '           <div class="random-post-author" id="random-post-author-two">\n' +
-          '        </div>\n' +
-          '      </div>' +
-          '</div>';
-        firstEl.appendChild(newEl);
-      });
-    }
-  });
-  extend(DiscussionList.prototype, 'oncreate', function () {
-    if (app.current.matches(IndexPage)) {
-
-      const charLim = app.forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
-
-      app.store
-        .find('posts', {
-          include: 'discussion,user',
-        })
-        .then((results) => {
-          m.redraw();
-          this.randomPost = results[Math.floor(Math.random() * results.length)];
-
-          const randomPostTitle = document.getElementById('random-post-text-three');
-          randomPostTitle.innerHTML = this.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g, '').substr(0, charLim);
-
-          const randomAuthor = document.getElementById('random-post-author-three');
-          const link = app.route.user(this.randomPost.user());
-          const user = this.randomPost.user().displayName();
-          const discussion = this.randomPost.discussion().data.attributes.title;
-          const discussionLink = app.route.discussion(this.randomPost.discussion());
-          randomAuthor.innerHTML = `<a href=${link}>${user}</a> in the <a href=${discussionLink}>${discussion}</a> `;
-        });
-
-      window.addEventListener('DOMContentLoaded', (event) => {
-        const elements = document.querySelectorAll('.DiscussionListItem');
-        const firstEl = elements.item(8); //
-        console.log(firstEl);
-
-        const newEl = document.createElement('div');
-        newEl.innerHTML =
-          '<div id="random-post-two"><div class="random-post-content">\n' +
-          '        <p class="random-post-text" id="random-post-text-three"></p>\n' +
-          '        <div class="random-post-info">\n' +
-          '           <div class="random-post-author" id="random-post-author-three">\n' +
-          '        </div>\n' +
-          '      </div>' +
-          '</div>';
-        firstEl.appendChild(newEl);
-      });
-    }
-  });
-  extend(DiscussionList.prototype, 'oncreate', function () {
-    if (app.current.matches(IndexPage)) {
-
-      const charLim = app.forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
-
-      app.store
-        .find('posts', {
-          include: 'discussion,user',
-        })
-        .then((results) => {
-          m.redraw();
-          this.randomPost = results[Math.floor(Math.random() * results.length)];
-
-          const randomPostTitle = document.getElementById('random-post-text-four');
-          randomPostTitle.innerHTML = this.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g, '').substr(0, charLim);
-
-          const randomAuthor = document.getElementById('random-post-author-four');
-          const link = app.route.user(this.randomPost.user());
-          const user = this.randomPost.user().displayName();
-          const discussion = this.randomPost.discussion().data.attributes.title;
-          const discussionLink = app.route.discussion(this.randomPost.discussion());
-          randomAuthor.innerHTML = `<a href=${link}>${user}</a> in the <a href=${discussionLink}>${discussion}</a> `;
-        });
-
-      window.addEventListener('DOMContentLoaded', (event) => {
-        const elements = document.querySelectorAll('.DiscussionListItem');
-        const firstEl = elements.item(12); //
-        console.log(firstEl);
-
-        const newEl = document.createElement('div');
-        newEl.innerHTML =
-          '<div id="random-post-two"><div class="random-post-content">\n' +
-          '        <p class="random-post-text" id="random-post-text-four"></p>\n' +
-          '        <div class="random-post-info">\n' +
-          '           <div class="random-post-author" id="random-post-author-four">\n' +
-          '        </div>\n' +
-          '      </div>' +
-          '</div>';
-        firstEl.appendChild(newEl);
-      });
-    }
-  });
-  extend(DiscussionList.prototype, 'oncreate', function () {
-    if (app.current.matches(IndexPage)) {
-
-      const charLim = app.forum.attribute('justoverclock-random-post.charLimitRandomPost') || 280;
-
-      app.store
-        .find('posts', {
-          include: 'discussion,user',
-        })
-        .then((results) => {
-          m.redraw();
-          this.randomPost = results[Math.floor(Math.random() * results.length)];
-
-          const randomPostTitle = document.getElementById('random-post-text-five');
-          randomPostTitle.innerHTML = this.randomPost.data.attributes.contentHtml.replace(/<\/?[^>]+(>|$)/g, '').substr(0, charLim);
-
-          const randomAuthor = document.getElementById('random-post-author-five');
-          const link = app.route.user(this.randomPost.user());
-          const user = this.randomPost.user().displayName();
-          const discussion = this.randomPost.discussion().data.attributes.title;
-          const discussionLink = app.route.discussion(this.randomPost.discussion());
-          randomAuthor.innerHTML = `<a href=${link}>${user}</a> in the <a href=${discussionLink}>${discussion}</a> `;
-        });
-
-      window.addEventListener('DOMContentLoaded', (event) => {
-        const elements = document.querySelectorAll('.DiscussionListItem');
-        const firstEl = elements.item(16); //
-        console.log(firstEl);
-
-        const newEl = document.createElement('div');
-        newEl.innerHTML =
-          '<div id="random-post-two"><div class="random-post-content">\n' +
-          '        <p class="random-post-text" id="random-post-text-five"></p>\n' +
-          '        <div class="random-post-info">\n' +
-          '           <div class="random-post-author" id="random-post-author-five">\n' +
           '        </div>\n' +
           '      </div>' +
           '</div>';
